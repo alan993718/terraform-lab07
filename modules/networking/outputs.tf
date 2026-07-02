daaf1f620bdb77ebcd69bc23702a9630a6c49b09
Alan Lai <alan993718@gmail.com>
2026-07-02 16:05:15 +0100

    fix: add missing location and subnet outputs to modules

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,11 +1,11 @@
 output "vnet_id" {
-
   value = azurerm_virtual_network.vnet.id
-
 }
 
 output "vnet_name" {
-
   value = azurerm_virtual_network.vnet.name
+}
 
+output "frontend_subnet_id" {
+  value = azurerm_subnet.subnet.id  # <-- Make sure this matches your resource name inside networking/main.tf
 }
